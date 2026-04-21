28e5a8ef39580520a3722cfb9e8103372886a301
Nathalie <106547743+natijade@users.noreply.github.com>
2026-04-21 21:36:53 +0100

    completed exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number' || !Number.isInteger(a) || !Number.isInteger(b) )
+    return "ERROR";
 
+    if (a > b) {
+        [a, b] = [b, a]
+    }
+
+    let finalSum = 0;
+
+    for (i = a; i <= b; i++){
+        finalSum += i;
+    }
+  return finalSum;
 };
 
 // Do not edit below this line
