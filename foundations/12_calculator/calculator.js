73eb201abc304f640a21ab7ca545a94e635b6bfb
Nathalie <106547743+natijade@users.noreply.github.com>
2026-04-28 16:26:19 +0100

    worked on calculator challenge

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,14 +1,13 @@
-const add = function() {
-	
-};
+const add = function(a, b) {
+  return a + b};
 
-const subtract = function() {
-	
-};
+const subtract = function(a, b) {
+  return a - b};
 
-const sum = function() {
-	
-};
+const sum = function(arr) {
+  for (){
+
+  }};
 
 const multiply = function() {
 
