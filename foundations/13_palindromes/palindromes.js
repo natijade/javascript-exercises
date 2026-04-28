fbea2a6f0b2613c39209d65a00bf4a4404d03a64
Nathalie <106547743+natijade@users.noreply.github.com>
2026-04-28 21:19:27 +0100

    Completed palindromes task

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const processedString = string
+.toLowerCase()
+.split('')
+.filter((character) => alphanumerical.includes(character))
+.join('');
+
+const reversedString = processedString.split('').reverse().join('');
+
+return reversedString === processedString
 
 };
+
+// .replace(/[^a-z0-9]/g, "")
 
 // Do not edit below this line
 module.exports = palindromes;
